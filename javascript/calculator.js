129d6c8535cbbd0059995982f45a5650a2dee710
Nandhukrishnanv2 <155341719+Nandhukrishnanv2@users.noreply.github.com>
2025-12-11 19:00:53 +0530

    day21-javascript class

---
+++ javascript/calculator.js
@@ -0,0 +1,14 @@
+let x = document.getElementById("");
+let y = document.getElementById("");
+let operation = document.getElementById("c");
+console.log(operation)
+switch(operation){
+    case "+":
+        let sum = x+y;
+    case "-":
+        let diff = x-y;
+    case "/":
+        let div = x/y;
+    case "*":
+        let mul = x*y;
+}
---
+++ javascript/dom.js
@@ -0,0 +1,7 @@
+// let title = document.getElementById("head1");
+// console.log("By ID ",title);
+// title.innerHTML <= "learn javascript";
+// title.style.color="blue";   
+
+let user = document.getElementById("name");
+user[0].style.border
